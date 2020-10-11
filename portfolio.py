f34f4c5005c8fe62209534f8b3d521dfe2c93d71
f1se4 <sergi.garcia.munoz@gmail.com>
2020-10-11 06:03:06 +0000

    apache gunicorn config files

--- portfolio.py
+++ portfolio.py
@@ -1,9 +1,28 @@
-from flask import Flask
+import sys
+from flask import Flask, render_template
+from flask_flatpages import FlatPages, pygments_style_defs
+from flask_frozen import Freezer
+
+POST_DIR = 'posts'
+FLATPAGES_ROOT = 'content'
+FLATPAGES_EXTENSION = '.md'
 
 app = Flask(__name__)
+flatpages = FlatPages(app)
+freezer = Freezer(app)
+app.config.from_object(__name__)
 
-def holamundo():
-	return 'Hola Mundo!'
+@app.route("/posts/")
+def posts():
+    posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
+    posts.sort(key=lambda item:item['date'], reverse=False)
+    return render_template('posts.html', posts=posts)
+
+@app.route('/posts/<name>/')
+def post(name):
+    path = '{}/{}'.format(POST_DIR, name)
+    post = flatpages.get_or_404(path)
+    return render_template('post.html', post=post)
 
 @app.route('/')
 def holamundo():
