de87a2344236e3f52b37195830f817697ffee7ff
f1se4 <52441535+f1se4@users.noreply.github.com>
2020-10-12 07:48:05 +0200

    .

--- portfolio.py
+++ portfolio.py
@@ -12,7 +12,6 @@ def home_page():
 
 @app.route('/curriculum')
 def curriculum():
-	file_path = url_for('static',filename='images/project3.jpg')
 	with open("./md/curriculum.md", "r") as file:
 		content = file.read()	
 	return render_template("curriculum.html", text=content)    
