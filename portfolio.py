832aca92ee4190202767e32414cb09852243d5a8
f1se4 <52441535+f1se4@users.noreply.github.com>
2020-10-12 08:20:54 +0200

    .

--- portfolio.py
+++ portfolio.py
@@ -1,10 +1,10 @@
 import sys
 from flask import Flask, render_template
-from flask_misaka import Misaka
+from flask_misaka import markdown
 
 
 app = Flask(__name__)
-Misaka(app,math_explicit=True)
+#Misaka(app, math_explicit=True)
 
 @app.route('/')
 def home_page():
@@ -14,13 +14,13 @@ def home_page():
 def curriculum():
 	with open("./md/curriculum.md", "r") as file:
 		content = file.read()	
-	return render_template("curriculum.html", text=content)
+	return render_template("curriculum.html", text=markdown(content,math_explicit=True))
 
 @app.route('/gompertz')
 def gompertz():
 	with open("./md/gompertz.md", "r") as file:
 		content = file.read()	
-	return render_template("gompertz.html", text=content)   	
+	return render_template("gompertz.html", text=markdown(content,math_explicit=True))  	
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
