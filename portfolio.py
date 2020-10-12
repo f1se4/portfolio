9d835717b4c0cd42ea9792d8ec15c99dc0b8ceaa
f1se4 <52441535+f1se4@users.noreply.github.com>
2020-10-12 09:41:38 +0200

    .

--- portfolio.py
+++ portfolio.py
@@ -1,5 +1,5 @@
 import sys
-from flask import Flask, render_template
+from flask import Flask, render_template, render_template_string
 import markdown
 
 md = markdown.Markdown(extensions=['mdx_math'])
@@ -13,14 +13,16 @@ def home_page():
 @app.route('/curriculum')
 def curriculum():
 	with open("./md/curriculum.md", "r") as file:
-		content = file.read()	
-	return render_template("curriculum.html", text=md.convert(content))
+		content = file.read()
+	content = render_template_string(md.convert(content))		
+	return render_template("curriculum.html", text=content)
 
 @app.route('/gompertz')
 def gompertz():
 	with open("./md/gompertz.md", "r") as file:
-		content = file.read()	
-	return render_template("gompertz.html", text=md.convert(content))  	
+		content = file.read()
+	content = render_template_string(md.convert(content))
+	return render_template("gompertz.html", text=content)  	
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
